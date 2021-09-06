14baaa575e49f7b3b774d86707e7469c478fac53
김창회 <dudnqnfqlc@naver.com>
2021-09-06 10:20:05 +0900

    css 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router';
 import styled from '@emotion/styled';
 import '../styles/index.css';
+import '../styles/highlight.css';
 import React, { createContext, useCallback, useEffect, useReducer, useState } from 'react';
 import Layout from 'antd/lib/layout/layout';
 import AppFooter from '../components/layouts/AppFooter';

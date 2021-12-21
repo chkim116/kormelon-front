1a5c63194a2a3781b013c3c381dcce470764406b
김창회 <dudnqnfqlc@naver.com>
2021-12-21 23:19:24 +0900

    feat migration to redux (auth logic)

--- src/store/reducer/auth.ts
+++ src/store/reducer/auth.ts
@@ -1,15 +1,30 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-interface InitialState {}
+interface Auth {
+  id: number;
+  username: string;
+  admin: boolean;
+}
 
-const initialState: InitialState = {};
+const initialState: Auth = {
+  username: '',
+  id: 0,
+  admin: false,
+};
 
 const auth = createSlice({
   initialState,
   name: 'auth',
-  reducers: {},
+  reducers: {
+    getAuth: (state, { payload }: PayloadAction<Auth>) => {
+      state = payload;
+    },
+    removeAuth: (state) => {
+      state = initialState;
+    },
+  },
 });
 
-export const {} = auth.actions;
+export const { getAuth, removeAuth } = auth.actions;
 
 export default auth.reducer;

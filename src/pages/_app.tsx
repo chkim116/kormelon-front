6acff1e841082656cd67504ab5ffa3e9f88d78f4
김창회 <dudnqnfqlc@naver.com>
2022-02-02 23:11:45 +0900

    /src 폴더 안으로 이사

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app';
-import { GlobalStyle } from 'styles/globalStyle';
+import { GlobalStyle } from 'src/styles/globalStyle';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
---
+++ src/pages/index.tsx
@@ -0,0 +1,3 @@
+import Home from 'src/views/home';
+
+export default Home;

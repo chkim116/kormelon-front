b595464987f741da20d81fdae628825f212853bf
김창회 <dudnqnfqlc@naver.com>
2022-03-01 21:40:05 +0900

    swc 모드 추가

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
-module.exports = {
-	swcMinify: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
 	reactStrictMode: true,
+	experimental: {
+		swcMinify: true,
+	},
 	images: {
 		domains: ['placehold.it'],
 	},
 };
+
+module.exports = nextConfig;

f6ee626383fbd7d6efd2f8c8aadabb0de17abe2f
kim changhoe <michaelkim@dreamyoungs.com>
2022-03-23 23:30:42 +0900

    메인 페이지 intro 삭제

--- src/views/home/HomeStyle.ts
+++ src/views/home/HomeStyle.ts
@@ -3,10 +3,6 @@ import styled from '@emotion/styled';
 const HomeStyle = styled.div`
 	width: 100%;
 	margin: 0 auto;
-
-	.description {
-		line-height: 24px;
-	}
 `;
 
 export default HomeStyle;
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -9,16 +9,6 @@ interface HomeProps {
 const Home = ({ posts }: HomeProps) => {
 	return (
 		<HomeStyle>
-			<div className='description'>
-				<p>Hello, My World.</p>
-				<p>This blog is open source.</p>
-				<p>If you wanna start blog project. check it out my repository</p>
-				{/* TODO: 레포 링크 */}
-				<a href='/' target='blank'>
-					blog-booster
-				</a>
-			</div>
-
 			<PostListTemplate posts={posts} />
 		</HomeStyle>
 	);

6111da90ce52504edf47746eb9b85b208a00982a
kim changhoe <dudnqnfqlc@naver.com>
2022-03-28 20:34:03 +0900

    image 관련 이슈 해결

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
+const withPlugins = require('next-compose-plugins');
+const withImages = require('next-images');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
+	images: {
+		disableStaticImages: true,
+	},
 };
 
-module.exports = nextConfig;
+module.exports = withPlugins(
+	[
+		[
+			withImages({
+				inlineImageLimit: false,
+				fileExtensions: ['jpg', 'jpeg', 'png', 'gif', 'svg'],
+			}),
+		],
+	],
+	nextConfig
+);

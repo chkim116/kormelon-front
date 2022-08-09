41e70042deaaa60495b6ce5811d2ff0098a31fa4
changhoe kim <dudnqnfqlc@naver.com>
2022-08-09 22:46:35 +0900

    feat: PostSearchBar 목업 컴포넌트

--- src/features/posts/components/PostMainImage.tsx
+++
@@ -1,3 +0,0 @@
-export const PostMainImage = () => {
-	return <div>PostMainImage</div>;
-};
--- src/features/posts/components/PostSearchBar.tsx
+++ src/features/posts/components/PostSearchBar.tsx
@@ -1,3 +1,33 @@
+import { SearchIcon } from '@chakra-ui/icons';
+import { Button, Flex, Heading, Input, SlideFade } from '@chakra-ui/react';
+
 export const PostSearchBar = () => {
-	return <div>PostSearchBar</div>;
+	return (
+		<SlideFade in={true}>
+			<Heading mb='6'>Find my articles.</Heading>
+			<Flex
+				as='form'
+				alignItems='center'
+				position='relative'
+				maxW={'540px'}
+				mb='2'
+			>
+				<Input
+					spellCheck={false}
+					placeholder='Search'
+					p={{ base: '4', md: '6' }}
+					borderRadius={24}
+				/>
+				<Button
+					position={'absolute'}
+					right='0'
+					p={{ base: '4', md: '6' }}
+					borderTopRightRadius={24}
+					borderBottomRightRadius={24}
+				>
+					<SearchIcon />
+				</Button>
+			</Flex>
+		</SlideFade>
+	);
 };
